c2f48c50352dd0329f0b7f73fadc99f2773a1ab0
JonasChai <jonas.chairutdinovas@gmail.com>
2023-08-23 12:03:10 +0300

    added team page

--- team.js
+++ team.js
@@ -1,5 +1,3 @@
-const log = (logs) => {console.log(logs)};
-
 const main = document.getElementsByTagName(`main`)[0];
 
 const diplayPlayer = (player) => {
@@ -9,20 +7,15 @@ const diplayPlayer = (player) => {
     return playerWrapper;
 }
 
-const openTeam = (team) => {
-    localStorage.setItem(`teamId`,team.id);
-    window.location.replace(`./team.html`);
-}
-
 const displayTeam = (team) => {
     const teamWrapper = document.createElement(`div`);
     teamWrapper.classList.add(`team-wrapper`);
-    teamWrapper.addEventListener(`click`, () => {
-        openTeam(team);
-    });
 
     const orgWrapper = document.createElement(`div`);
     orgWrapper.classList.add(`org-wrapper`);
+
+    const teamInfoWrapper = document.createElement(`div`);
+    teamInfoWrapper.classList.add(`team-info-wrapper`);
 
     const teamLogo = document.createElement(`img`);
     teamLogo.src = team.logo;
@@ -30,49 +23,40 @@ const displayTeam = (team) => {
     const teamName = document.createElement(`h2`);
     teamName.innerText = team.name;
 
-    orgWrapper.append(teamLogo,teamName);
+    
 
     const teamRegion = document.createElement(`div`);
     teamRegion.innerText = `Region: ${team.region}` ;
 
-    teamWrapper.append(orgWrapper);
-    teamWrapper.append(teamRegion);
 
     const playersWrapper = document.createElement(`div`);
     playersWrapper.classList.add(`players-wrapper`);
+    playersWrapper.innerText = `Players:`;
 
     team.players.forEach((player) => {
         playersWrapper.append(diplayPlayer(player));
     })
-    
-    teamWrapper.append(playersWrapper);     
-    
+        
     const teamCoach = document.createElement(`div`);
     teamCoach.innerText = `Coach: ${team.coach || `vacant`}`;
-    teamWrapper.append(teamCoach);
+    
 
     const establishedWrapper = document.createElement(`div`);
     establishedWrapper.innerText = `Established: ` + new Date(team.established).getFullYear();
-
     
-    teamWrapper.append(establishedWrapper);
+    orgWrapper.append(teamLogo,teamName);
+    teamInfoWrapper.append(teamRegion,playersWrapper,teamCoach,establishedWrapper);
+    
+    teamWrapper.append(orgWrapper);
+    teamWrapper.append(teamInfoWrapper);
 
     return teamWrapper;
 }
 
-const displayTeams = (teams) =>{
-    const teamsWrapper = document.createElement(`div`);
-    teamsWrapper.classList.add(`teams-wrapper`);
-    teams.forEach((team) => {
-        teamsWrapper.append(displayTeam(team));
-    });
-    return teamsWrapper;
-}
-
 const getData = async () => {
-    const response = await fetch(`https://64e32b8abac46e480e784eb4.mockapi.io/team`);
-    const teams = await response.json();
-    main.append(displayTeams(teams));
+    const response = await fetch(`https://64e32b8abac46e480e784eb4.mockapi.io/team/` + localStorage.teamId);
+    const team = await response.json();
+    main.append(displayTeam(team));
 }
 
 getData();
